13ac2761e8a3a3bc9d596c6f666066df37047669
Raynne Nogueira <raynnenogueira123@hotmail.com>
2023-04-13 11:27:10 -0300

    add js

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ Esse foi um trabalho proposto para a disciplina de Codigos Web Coding
 <div style="display: inline_block">
   <img align="center" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
   <img align="center" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+  <img align="center" alt="Bruno-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
 </div>
 
 <br>
---
+++ script.js
@@ -0,0 +1,35 @@
+
+var button = document.getElementById("botaoLogin");
+
+function passarMouse() {
+    botaoLogin.style.backgroundColor = "#fc6843";
+}
+
+function retirarMouse() {
+    botaoLogin.style.backgroundColor = "#f95127";
+}
+
+
+
+var button = document.getElementById("botaoForm");
+
+function passarForm() {
+    botaoForm.style.backgroundColor = "#fc6843";
+}
+
+function retirarForm() {
+    botaoForm.style.backgroundColor = "#f95127";
+}
+
+
+var button = document.getElementById("botaoCardp");
+
+function passarCardp() {
+    botaoCardp.style.backgroundColor = "#fc6843";
+}
+
+function retirarCardp() {
+    botaoCardp.style.backgroundColor = "#f95127";
+}
+
+
